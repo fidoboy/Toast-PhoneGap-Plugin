660dd5d2243927e28572211fb5a8ddf0572ab752
fidoboy <fidoboy@users.noreply.github.com>
2014-12-20 17:37:40 +0100

    Update Toast+UIView.h

--- src/ios/Toast+UIView.h
+++ src/ios/Toast+UIView.h
@@ -5,9 +5,9 @@
 // each makeToast method creates a view and displays it as toast
 - (void)makeToast:(NSString *)message;
 - (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position;
-- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position image:(UIImage *)image;
+- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position image:(NSString *)image;
 - (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position title:(NSString *)title;
-- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position title:(NSString *)title image:(UIImage *)image;
+- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position title:(NSString *)title image:(NSString *)image;
 
 // displays toast with an activity spinner
 - (void)makeToastActivity;
